453a6d16a6a21fbc5e6ec6f55c13fb0e8f0f6357
gui21657 <gui21657@uvg.edu.gt>
2024-08-15 22:51:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,115 @@
 console.log('Chart.js version:', Chart.version);
 
+// Manejo de evento para el cambio en el tipo de gradiente
+document.getElementById('tipoGradiente').addEventListener('change', function() {
+    // Restablecer los valores de los inputs
+    document.getElementById('aInput').value = '';
+    document.getElementById('gInput').value = '';
+    document.getElementById('iInput').value = '';
+    document.getElementById('nInput').value = '';
+    document.getElementById('GInput').value = '';
+    document.getElementById('output').textContent = ''; // Limpiar el resultado
+    if (window.cashFlowChart && typeof window.cashFlowChart.destroy === 'function') {
+        window.cashFlowChart.destroy();
+        window.cashFlowChart = null; // Asegurarse de que se puede crear un nuevo gráfico más tarde
+    }
+    const tipo = this.value;
+
+    // Ocultar todos los grupos
+    document.getElementById('aGroup').style.display = 'none';
+    document.getElementById('gGroup').style.display = 'none';
+    document.getElementById('iGroup').style.display = 'none';
+    document.getElementById('nGroup').style.display = 'none';
+    document.getElementById('GGroup').style.display = 'none';
+
+    // Mostrar los grupos según la selección
+    if (['geometrico'].includes(tipo)) {
+        document.getElementById('aGroup').style.display = 'block';
+        document.getElementById('gGroup').style.display = 'block';
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        
+    }
+    if (['aritmetico'].includes(tipo)) {
+        document.getElementById('aGroup').style.display = 'block';
+        
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        
+    }
+    if (['FuturoCompuesto'].includes(tipo)) {
+        document.getElementById('aGroup').style.display = 'block';
+        
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        
+    }
+    if (['FuturoSimple'].includes(tipo)) {
+        document.getElementById('aGroup').style.display = 'block';
+        
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        
+    }
+    if (['AnualidadAritmetico'].includes(tipo)) {
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        document.getElementById('GGroup').style.display = 'block';
+    }
+    if (['FuturoGeometrico'].includes(tipo)) {
+        document.getElementById('aGroup').style.display = 'block';
+        document.getElementById('gGroup').style.display = 'block';
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        
+    }
+    if (['FuturoAritmetico'].includes(tipo)) {
+
+        document.getElementById('iGroup').style.display = 'block';
+        document.getElementById('nGroup').style.display = 'block';
+        document.getElementById('GGroup').style.display = 'block';
+    }
+
+});
+
+// Ejecutar la función al cargar la página para ajustar la vista inicial
+document.getElementById('tipoGradiente').dispatchEvent(new Event('change'));
+
+// Manejo de evento para el botón de cálculo
 document.getElementById('calcularBtn').addEventListener('click', function() {
     const tipo = document.getElementById('tipoGradiente').value;
     const a = parseFloat(document.getElementById('aInput').value);
     const g = parseFloat(document.getElementById('gInput').value);
     const i = parseFloat(document.getElementById('iInput').value);
     const n = parseInt(document.getElementById('nInput').value);
-
+    const G = parseInt(document.getElementById('GInput').value);
+    
     let resultado = 0;
     let cashFlow = [];
 
+    // Uso de la fórmula correspondiente según el tipo seleccionado
     if (tipo === 'geometrico') {
-        resultado = calcularFactorPresenteGeometrico(a, g, i, n);
-        document.getElementById('output').textContent = `Valor presente del gradiente geométrico: ${resultado.toFixed(2)}`;
-        cashFlow = calcularFlujoGeometrico(a, g, i, n);
+        resultado = formulas['geometrico'](a, g, i, n, G);
     } else if (tipo === 'aritmetico') {
-        resultado = calcularFactorPresenteAritmetico(a, i, n);
-        document.getElementById('output').textContent = `Valor presente del gradiente aritmético: ${resultado.toFixed(2)}`;
-        cashFlow = calcularFlujoAritmetico(a, i, n);
+        resultado = formulas['aritmetico'](a, g, i, n, G);
+    } else if (tipo === 'FuturoCompuesto') {
+        resultado = formulas['FuturoCompuesto'](a, i, n);
+    } else if (tipo === 'FuturoSimple') {
+        resultado = formulas['FuturoSimple'](a, g, i, n, G);
+    } else if (tipo === 'AnualidadAritmetico') {
+        resultado = formulas['AnualidadAritmetico'](a, g, i, n, G);
+    } else if (tipo === 'FuturoGeometrico') {
+        resultado = formulas['FuturoGeometrico'](a, g, i, n);
+    } else if (tipo === 'FuturoAritmetico') {
+        resultado = formulas['FuturoAritmetico']( i, n, G);
+    } else {
+        console.error('Tipo de gradiente no reconocido.');
+    }
+
+    document.getElementById('output').textContent = `Resultado: ${resultado.toFixed(2)}`;
+
+    if (formulas[tipo]) {
+        cashFlow = calcularFlujo(tipo, a, g, i, n, G, resultado);
     }
 
     console.log('Flujo de efectivo:', cashFlow);
@@ -29,64 +121,105 @@ document.getElementById('calcularBtn').addEventListener('click', function() {
     }
 });
 
-function calcularFactorPresenteGeometrico(a, g, i, n) {
-    const gDecimal = g / 100;
-    const iDecimal = i / 100;
+// Diccionario de fórmulas
+const formulas = {
+    'geometrico': function(a, g, i, n, G) {
+        const gDecimal = g / 100;
+        const iDecimal = i / 100;
+
+        if (iDecimal === gDecimal) {
+            return (n * a) / (1 + iDecimal);
+        }
+
+        return a * ((1 - Math.pow(1 + gDecimal, n) * Math.pow(1 + iDecimal, -n)) / (iDecimal - gDecimal));
+    },
+    'aritmetico': function(a, g, i, n, G) {
+        const iDecimal = i / 100;
+
+        return a * ((Math.pow(1 + iDecimal, n) - iDecimal * n - 1) / (Math.pow(iDecimal, 2) * Math.pow(1 + iDecimal, n)));
+    },
+    'FuturoCompuesto': function(a, i, n) {
+        const iDecimal = i / 100;
+        return a * Math.pow(1 + iDecimal, n); // interés compuesto
+    },
+    'FuturoSimple': function(a, g, i, n, G) {
+        const iDecimal = i / 100;
+        return (a * iDecimal * n) + a; // interés simple
+    },
+    'AnualidadAritmetico': function(a, g, i, n, G) {
+        const iDecimal = i / 100;
+        return G * ((1 / iDecimal) - (n / (Math.pow(1 + iDecimal, n) - 1)));
+    },
+    'FuturoGeometrico': function(a, g, i, n) {
+        const gDecimal = g / 100;
+        const iDecimal = i / 100;
+        
+        if (gDecimal !== iDecimal) {
+            return a * ((Math.pow(1 + gDecimal, n) - Math.pow(1 + iDecimal, n)) / (gDecimal - iDecimal));
+        } else {
+            // Caso especial cuando g == i
+            return n * a * Math.pow(1 + iDecimal, n - 1);
+        }
+    },
+    'FuturoAritmetico': function( i, n, G) {
+        const iDecimal = i / 100;  // Convertir la tasa de interés a decimal
     
-    if (iDecimal === gDecimal) {
-        return (n * a) / (1 + iDecimal);
+        // Calcular el valor futuro del gradiente aritmético
+        return G * (((Math.pow(1 + iDecimal, n) - 1) / iDecimal) - n);
     }
-    
-    return a * ((1 - Math.pow(1 + gDecimal, n) * Math.pow(1 + iDecimal, -n)) / (iDecimal - gDecimal));
-}
+};
 
-function calcularFactorPresenteAritmetico(a, i, n) {
-    const iDecimal = i / 100;
-    
-    return a * ((Math.pow(1 + iDecimal, n) - iDecimal * n - 1) / (Math.pow(iDecimal, 2) * Math.pow(1 + iDecimal, n)));
-}
-
-function calcularFlujoGeometrico(a, g, i, n) {
+function calcularFlujo(tipo, a, g, i, n, G, resultadoFinal) {
     let cashFlow = [];
-    let valor = a;
-    for (let i = 0; i < n; i++) {
+    
+    // Registrar el primer valor como el valor inicial (positivo)
+    cashFlow.push(-a , -G);
+
+    // Luego, calcular el flujo para los periodos subsecuentes usando la fórmula correspondiente
+    for (let j = 1; j <= n; j++) {
+        let valor;
+        if (tipo === 'geometrico') {
+            valor = formulas['geometrico'](a, g, i, j);  // Reemplaza n con j
+        } else if (tipo === 'aritmetico') {
+            valor = formulas['aritmetico'](a, g, i, j);  // Reemplaza n con j
+        } else if (tipo === 'FuturoCompuesto') {
+            valor = formulas['FuturoCompuesto'](a, i, j);   // Reemplaza n con j
+        } else if (tipo === 'FuturoSimple') {
+            valor = formulas['FuturoSimple'](a, g, i, j, G);   // Reemplaza n con j
+        } else if (tipo === 'AnualidadAritmetico') {
+            valor = formulas['AnualidadAritmetico'](a, g, i, j, G);   // Reemplaza n con j
+        } else if (tipo === 'FuturoGeometrico') {
+            valor = formulas['FuturoGeometrico'](a, g, i, j);   // Reemplaza n con j
+        } else if (tipo === 'FuturoAritmetico') {
+            valor = formulas['FuturoAritmetico']( i, j, G);   // Reemplaza n con j
+        }
+        
         cashFlow.push(valor);
-        valor *= (1 + g / 100);
     }
+
     return cashFlow;
 }
 
-function calcularFlujoAritmetico(a, i, n) {
-    let cashFlow = [];
-    let valor = a;
-    for (let i = 0; i < n; i++) {
-        cashFlow.push(valor);
-        valor += i;
-    }
-    return cashFlow;
-}
-
+// Función para mostrar el gráfico
 function mostrarGrafico(cashFlow) {
     const canvasElement = document.getElementById('cashFlowChart');
     
     if (canvasElement) {
         const ctx = canvasElement.getContext('2d');
         
-        // Verificar si ya existe un gráfico, y destruirlo si es necesario
         if (window.cashFlowChart && typeof window.cashFlowChart.destroy === 'function') {
             window.cashFlowChart.destroy();
         }
 
-        // Crear el nuevo gráfico
         window.cashFlowChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: cashFlow.map((_, index) => `Periodo ${index + 1}`),
+                labels: cashFlow.map((_, index) => `Periodo ${index}`),
                 datasets: [{
                     label: 'Flujo de Efectivo',
                     data: cashFlow,
-                    backgroundColor: 'rgba(116, 235, 213, 0.5)',
-                    borderColor: 'rgba(116, 235, 213, 1)',
+                    backgroundColor: cashFlow.map(value => value < 0 ? 'rgba(255, 99, 132, 0.5)' : 'rgba(116, 235, 213, 0.5)'),
+                    borderColor: cashFlow.map(value => value < 0 ? 'rgba(255, 99, 132, 1)' : 'rgba(116, 235, 213, 1)'),
                     borderWidth: 1
                 }]
             },
@@ -98,7 +231,6 @@ function mostrarGrafico(cashFlow) {
                             callback: function(value) {
                                 return new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'USD' }).format(value);
                             },
-                            min: 0,
                         }
                     }
                 },
@@ -110,4 +242,3 @@ function mostrarGrafico(cashFlow) {
         console.error('No se pudo encontrar el elemento canvas con el ID cashFlowChart.');
     }
 }
- 
